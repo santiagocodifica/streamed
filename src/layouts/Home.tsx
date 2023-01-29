eee60c5a65a4161bb679bbadb31cd7b45a46b317
Santiago <santiagopm36@gmail.com>
2023-01-29 00:31:10 -0300

    v5

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return(
     <main class="w-screen h-screen grid place-items-center">
-      <h2 class="w-full p-6 text-4xl text-center md:w-1/2 md:text-5xl">Search and find where to stream any movie or tv show</h2>
+      <h2 class="w-full p-6 text-4xl text-center md:w-1/2 md:text-5xl">Search and find where to stream any movie or tv show {process.env.COUNTER}</h2>
     </main>
   )
 }
